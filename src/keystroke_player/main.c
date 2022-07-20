d89eb1c99af31bdec904e49a420d483e22fff00e
CornSnek <jdaffon123@gmail.com>
2022-07-20 00:28:49 -0700

    Added VT_Delay and VT_RepeatEnd

--- src/keystroke_player/main.c
+++ src/keystroke_player/main.c
@@ -9,8 +9,7 @@ void run_program(command_array_t* cmd_arr);
 int main(int argc, char** argv){
     (void)argc;
     (void)argv;
-    printf("Making a keystroke player in c.\n");
-    char a_string_stack[]="w=d,a=d,s=d.s1;d=d.s2;q=d.0";
+    char a_string_stack[]="(A;w=d,a=d,s=d.s1;d=d.s2;q=d,a=u.0;)A=2";
     char* a_string_heap=(char*)malloc(sizeof(a_string_stack)/sizeof(char));
     strcpy(a_string_heap,a_string_stack);
     shared_string_manager* ssm=SSManager_new();
@@ -52,26 +51,32 @@ void run_program(command_array_t* cmd_arr){
         switch(cmd_type){
             case(VT_KeyStroke):
                 if(cmd_u.ks.key_state){
+                    printf("Command #%d: Key down for %s\n",cmd_arr_i,cmd_u.ks.key);
                     xdo_send_keysequence_window_down(xdo_obj,focus_window,cmd_u.ks.key,0);
                     key_down_check_add(kdt,cmd_u.ks.key);
                 }
                 else{
+                    printf("Command #%d: Key up for %s\n",cmd_arr_i,cmd_u.ks.key);
                     xdo_send_keysequence_window_up(xdo_obj,focus_window,cmd_u.ks.key,0);
                     key_down_check_remove(kdt,cmd_u.ks.key);
                 }
                 break;
             case(VT_Delay):
+                printf("Command #%d: Sleeping for %ld microseconds\n",cmd_arr_i,cmd_u.delay);
                 usleep(cmd_u.delay);
                 break;
             case(VT_RepeatEnd):
-                printf("TODO\n");
+                int* rst_counter=&(cmd_arr->cmds[cmd_u.repeat_end.index].cmd.repeat_start.counter);
+                printf("Command #%d: Jump to Command #%d if Counter %d<%d\n",cmd_arr_i,cmd_u.repeat_end.index,++(*rst_counter),cmd_u.repeat_end.counter_max);
+                if(*rst_counter!=cmd_u.repeat_end.counter_max) cmd_arr_i=cmd_u.repeat_end.index;//Go back if not counter_max
                 break;
             default:
                 break;
         }
         if(++cmd_arr_i==cmd_arr_len) break;
     }
-    if(new_window){//Disable keys if on new window.
+    if(new_window!=focus_window){//Disable keys if on new window.
+        printf("Switched to new window. Disabling autoclicker.\n");
         xdo_focus_window(xdo_obj,new_window);
         key_down_check_key_up(kdt,xdo_obj,new_window);
     }
